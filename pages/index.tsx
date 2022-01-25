37eb1f9dba8d16c90cf452d95bdc07d0f1818620
Kielx <kiel66@gmail.com>
2022-01-25 18:30:04 +0100

    Add TailwindCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ const Home: NextPage = () => (
         <meta name="description" content="Chris Pantak tech blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h2 className="bg-red-500 text-bold text-gray-100">Hello</h2>
+      
+
     </div>
   )
 
