8439a452c8a4124cad0e781d444da5c797305ddf
Kielx <kiel66@gmail.com>
2022-01-26 23:18:47 +0100

    Add links to posts

--- components/MainPost.tsx
+++ components/MainPost.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {
   slug: string
@@ -16,35 +17,37 @@ const MainPost: React.FC<Props> = ({
   excerpt,
   date,
 }) => (
-  <div
-    className="Post group col-span-12 mainCard w-full min-h-[40vh] flex bg-white rounded-xl shadow-sm border border-opacity-5 border-black cursor-pointer"
-    key={slug}
-  >
-    <div className="flex w-1/2 overflow-hidden rounded-xl">
-      <div className="w-full relative cardImageContainer">
-        <Image
-          src={coverImage}
-          alt={title}
-          layout="fill"
-          objectFit="cover"
-          objectPosition="bottom"
-          className="rounded-l-xl"
-        />
-        <div className="overlay" />
+  <Link href={`/posts/${slug}`} passHref>
+    <div
+      className="Post group col-span-12 mainCard w-full min-h-[40vh] flex bg-white rounded-xl shadow-sm border border-opacity-5 border-black cursor-pointer"
+      key={slug}
+    >
+      <div className="flex w-1/2 overflow-hidden rounded-xl">
+        <div className="w-full relative cardImageContainer">
+          <Image
+            src={coverImage}
+            alt={title}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="bottom"
+            className="rounded-l-xl"
+          />
+          <div className="overlay" />
+        </div>
+      </div>
+
+      <div className="bg-white shadow-none rounded-xl p-8 w-1/2 flex flex-col">
+        <h3 className="text-3xl font-extrabold text-center">{title}</h3>
+
+        <p className="transition-all group-hover:text-gray-600  text-gray-500 text-xl  pt-8">
+          {excerpt}
+        </p>
+        <p className="transition-all group-hover:text-gray-600  text-gray-500 text-sm mt-auto text-right">
+          {date}
+        </p>
       </div>
     </div>
-
-    <div className="bg-white shadow-none rounded-xl p-8 w-1/2 flex flex-col">
-      <h3 className="text-3xl font-extrabold text-center">{title}</h3>
-
-      <p className="transition-all group-hover:text-gray-600  text-gray-500 text-xl  pt-8">
-        {excerpt}
-      </p>
-      <p className="transition-all group-hover:text-gray-600  text-gray-500 text-sm mt-auto text-right">
-        {date}
-      </p>
-    </div>
-  </div>
+  </Link>
 )
 
 export default MainPost
--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {
   slug: string
@@ -10,34 +11,36 @@ type Props = {
 }
 
 const Post: React.FC<Props> = ({ slug, coverImage, title, excerpt, date }) => (
-  <div
-    className="Post group col-span-6 bg-white rounded-xl shadow-sm cursor-pointer relative border border-opacity-5 border-black"
-    key={slug}
-  >
-    <div className="flex w-full h-64 overflow-hidden rounded-t-xl">
-      <div className="w-full relative cardImageContainer">
-        <Image
-          src={coverImage}
-          alt={title}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-t-xl"
-        />
-        <div className="overlay" />
+  <Link href={`/posts/${slug}`} passHref>
+    <div
+      className="Post group col-span-6 bg-white rounded-xl shadow-sm cursor-pointer relative border border-opacity-5 border-black"
+      key={slug}
+    >
+      <div className="flex w-full h-64 overflow-hidden rounded-t-xl">
+        <div className="w-full relative cardImageContainer">
+          <Image
+            src={coverImage}
+            alt={title}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-t-xl"
+          />
+          <div className="overlay" />
+        </div>
+      </div>
+
+      <div className="bg-white shadow-none rounded-xl p-8 pb-10 flex flex-col">
+        <h3 className="text-2xl font-extrabold indent-2">{title}</h3>
+
+        <p className="transition-all group-hover:text-gray-600 text-gray-500 text-lg  pt-4">
+          {excerpt}
+        </p>
+        <p className="transition-all group-hover:text-gray-600  text-gray-500 text-sm absolute right-6 bottom-4">
+          {date}
+        </p>
       </div>
     </div>
-
-    <div className="bg-white shadow-none rounded-xl p-8 pb-10 flex flex-col">
-      <h3 className="text-2xl font-extrabold indent-2">{title}</h3>
-
-      <p className="transition-all group-hover:text-gray-600 text-gray-500 text-lg  pt-4">
-        {excerpt}
-      </p>
-      <p className="transition-all group-hover:text-gray-600  text-gray-500 text-sm absolute right-6 bottom-4">
-        {date}
-      </p>
-    </div>
-  </div>
+  </Link>
 )
 
 export default Post
