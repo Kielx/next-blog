7725b830d2c0aa9f6928817000800f43723251b9
Kielx <kiel66@gmail.com>
2022-01-27 19:32:13 +0100

    Add responsive header with custom hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,91 @@
 import Image from 'next/image'
-import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="w-full h-24 flex bg-[#2C2C2C] pb-4  ">
-      <Link href="/" passHref>
-        <div className="h-full w-1/3 relative cursor-pointer">
-          <Image src="/logoDark.svg" alt="logo" layout="fill" />
-        </div>
-      </Link>
-      <div className="h-full w-1/2 flex justify-end items-center gap-8 text-lg font-bold text-[#EEEEEE]">
+    <>
+      <div className="md:hidden w-full h-24 flex flex-wrap bg-[#2C2C2C] py-4  ">
         <Link href="/" passHref>
-          <div className="hover:text-gray-400 cursor-pointer transition-all">
-            Home
+          <div className="h-full w-1/2 relative cursor-pointer">
+            <Image src="/logoDark.svg" alt="logo" layout="fill" />
           </div>
         </Link>
-        <Link href="/about" passHref>
-          <div className="hover:text-gray-400 cursor-pointer transition-all">
-            About
-          </div>
-        </Link>
-        <Link href="/contact" passHref>
-          <div className="hover:text-gray-400 cursor-pointer transition-all">
-            Contact
-          </div>
-        </Link>
-        <a
-          className="hover:text-gray-400 cursor-pointer transition-all"
-          href="https://pantak.net"
+        <div className="w-1/2 flex items-center justify-items-end">
+          <button
+            type="button"
+            onClick={() => setOpen(!open)}
+            className="hamburger ml-auto text-right cursor-pointer font-bold text-white text-2xl pr-6"
+          >
+            <div id="nav-icon1" className={`${open ? 'open' : ''}`}>
+              <span />
+              <span />
+              <span />
+            </div>
+          </button>
+        </div>
+        <div
+          className={`${
+            open ? 'max-h-[100vh] opacity-100' : 'max-h-0 opacity-0 invisible'
+          }  z-20 mobileMenu w-full justify-end items-center gap-2 py-4 text-lg font-bold text-[#EEEEEE] bg-[#2C2C2C]`}
         >
-          Portfolio
-        </a>
+          <Link href="/" passHref>
+            <div className="hover:text-gray-400 cursor-pointer transition-all">
+              Home
+            </div>
+          </Link>
+          <Link href="/about" passHref>
+            <div className="hover:text-gray-400 cursor-pointer transition-all">
+              About
+            </div>
+          </Link>
+          <Link href="/contact" passHref>
+            <div className="hover:text-gray-400 cursor-pointer transition-all">
+              Contact
+            </div>
+          </Link>
+          <a
+            className="hover:text-gray-400 cursor-pointer transition-all"
+            href="https://pantak.net"
+          >
+            Portfolio
+          </a>
+        </div>
       </div>
-    </div>
+
+      <div className="hidden w-full h-24 md:flex bg-[#2C2C2C] py-4  ">
+        <Link href="/" passHref>
+          <div className="h-full w-1/3 relative cursor-pointer">
+            <Image src="/logoDark.svg" alt="logo" layout="fill" />
+          </div>
+        </Link>
+        <div className="h-full w-1/2 flex justify-end items-center gap-8 text-lg font-bold text-[#EEEEEE]">
+          <Link href="/" passHref>
+            <div className="hover:text-gray-400 cursor-pointer transition-all">
+              Home
+            </div>
+          </Link>
+          <Link href="/about" passHref>
+            <div className="hover:text-gray-400 cursor-pointer transition-all">
+              About
+            </div>
+          </Link>
+          <Link href="/contact" passHref>
+            <div className="hover:text-gray-400 cursor-pointer transition-all">
+              Contact
+            </div>
+          </Link>
+          <a
+            className="hover:text-gray-400 cursor-pointer transition-all"
+            href="https://pantak.net"
+          >
+            Portfolio
+          </a>
+        </div>
+      </div>
+    </>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+
 import '@fontsource/montserrat/700.css'
 import '@fontsource/montserrat/500.css'
 import '@fontsource/montserrat/600.css'
 import '@fontsource/montserrat/400.css'
 import 'animate.css'
 import Header from '../components/Header'
+import '../styles/header.css'
+import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
