bb49da5d001e663f2b641164fc6d568c1f7b09d1
Kielx <kiel66@gmail.com>
2022-02-06 23:42:14 +0100

    Add contact form: 1. Contact form template 2. Contact form api page 3. Rate limiting 4. Mails with sendgrid Twilio 5. Validating and sanitizing input 6.Honeypot for bots

---
+++ pages/api/contact.ts
@@ -0,0 +1,78 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+import { NextApiRequest, NextApiResponse } from 'next'
+import requestIp from 'request-ip'
+import rateLimit from 'express-rate-limit'
+import sgMail from '@sendgrid/mail'
+import validator from 'validator'
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+const ipMiddleware = function (req: NextApiRequest) {
+  const clientIp = requestIp.getClientIp(req)
+  return clientIp
+}
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: ipMiddleware,
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const body = JSON.parse(req.body)
+  // Check honeypot field value to see if it's empty
+
+  await runMiddleware(req, res, limiter)
+
+  const email = validator.isEmail(body.email)
+    ? validator.normalizeEmail(body.email)
+    : ''
+  const message = validator.escape(body.message).trim()
+  const name = validator.escape(body.name).trim()
+  if (body.bot || !email || !message || !name) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Bad request or missing fields',
+    })
+  } else {
+    // Handle the request
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: process.env.EMAIL_TO, // Change to your recipient
+      from: process.env.EMAIL_FROM, // Change to your verified sender
+      subject: `Blog contact request from ${name} at ${email}`,
+      text: `${message}`,
+      html: `${message}`,
+    }
+    try {
+      sgMail.send(msg)
+      res.status(200).json({ message: 'Message successfully sent' })
+    } catch (error) {
+      res.status(500).json({ error })
+    }
+  }
+}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,78 @@
 import type { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useState } from 'react'
 import Header from '../components/Header'
 
 const Contact = () => {
   const { t } = useTranslation('contact')
+  const [state, setState] = useState({})
+
+  const handleChange = (e: React.ChangeEvent<EventTarget>) => {
+    const target = e.target as HTMLInputElement
+    setState({ ...state, [target.name]: target.value })
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    const url = `/api/contact`
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(state),
+    })
+  }
 
   return (
     <>
       <Header />
       <h1>{t('h1')}</h1>
+      <form
+        name="contact"
+        method="post"
+        action="/contact-us"
+        onSubmit={handleSubmit}
+      >
+        <p hidden>
+          <label htmlFor="bot">
+            Don’t fill this out: <input name="bot" onChange={handleChange} />
+          </label>
+        </p>
+        <input type="hidden" name="form-name" value="contact" />
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            placeholder="Name"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Your Email Address
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="E-Mail address"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="message">
+          Message
+          <textarea
+            id="message"
+            name="message"
+            required
+            placeholder="Your message"
+            rows={3}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Send</button>
+      </form>
     </>
   )
 }
