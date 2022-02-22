e260b25f77a8dd2dd4bca637015ac22139e092df
Kielx <kiel66@gmail.com>
2022-02-22 18:40:18 +0100

    Fix errors with line higlight.
    First fixed error was occuring because line highglight was generated anyway because of 'false' instead of 'null'
    
    Second error was caused by padding on top, making the highglight span one line too many

--- components/ReactMarkdownLineHighlight.tsx
+++ components/ReactMarkdownLineHighlight.tsx
@@ -3,7 +3,7 @@
 const ReactMarkdownLineHighlight = (props: any) => {
   const { children, node } = props
   return (
-    <pre {...props} data-line={node?.children[0]?.data?.meta || false}>
+    <pre {...props} data-line={node?.children[0]?.data?.meta || null}>
       {children}
     </pre>
   )
