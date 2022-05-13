953620eddde7507278bd794c3192dd0e186f1745
Kielx <kiel66@gmail.com>
2022-05-13 11:59:08 +0200

    Update about page removing gif, update README

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
+# Next-Blog
+
 [![Cypress Tests](https://github.com/Kielx/next-blog/actions/workflows/main.yml/badge.svg)](https://github.com/Kielx/next-blog/actions/workflows/main.yml)
 
-# Todo
+## Todo
 
-- [ ] Move header link styles from globals to header.module.css
 - [x] ~~Favicons~~
 - [x] ~~New meta name~~
 - [x] ~~Style contact page~~
 - [x] ~~Style about me page~~
 - [x] ~~Add new posts with content~~
-- [ ] Google gtag
-- [x] Add space between mobile menu items
-- [ ] Remove about me gif
+- [x] ~~Add space between mobile menu items~~
+- [x] ~~Remove about me gif~~
 - [x] ~~Fix blocking od sług pages (branch)~~
 - [x] ~~Add button to go back to top of the page~~
-- [ ] Add footer
+- [x] ~~Add CSS ribbon to projects and maybe blogs cards~~
 - [x] ~~Add links between parts of tutorial~~
+- [ ] Add footer
+- [ ] Move header link styles from globals to header.module.css
 - [ ] Add projects
 - [ ] Add search functionality for posts and projects
 - [ ] Add searching via keyword
-- [ ] Add footer
 - [ ] Add a little more in the about me section
 - [ ] Rework PostCard, MiniPostCard, MainPostCard to be only one component using separate styles
 - [ ] Update Meta tags images to fit Pantak.net brand
 - [ ] Update meta title in Head Component and ```[Slug]``` components to fit Pantak.net
-- [ ] Add CSS ribbon to projects and maybe blogs cards
--- pages/about.tsx
+++ pages/about.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Image from 'next/image'
 import Header from '../components/Header'
+import heroImage from '../public/myPhoto.webp'
 
 const About = () => {
   const { t } = useTranslation('about')
@@ -10,31 +11,28 @@ const About = () => {
   return (
     <div className="min-h-full bg-white">
       <Header />
-      <div className="m-auto w-full max-w-[692px] flex-col items-center justify-items-center gap-20 px-4 py-8 xs:px-8 md:px-0 md:py-20 lg:flex lg:flex-row xl:max-w-[980px]">
-        <div className="relative m-auto h-[250px] w-[250px] flex-shrink-0 flex-grow-0 rounded-md border-4 border-white shadow-md">
-          <Image
-            src="/myPhoto.webp"
-            alt="about"
-            width={250}
-            height={250}
-            className="rounded-md"
-          />
-        </div>
+      <div className="m-auto w-full max-w-[692px] flex-col items-center justify-items-center  px-4 py-8 xs:px-8 md:px-0 md:py-10 lg:flex lg:flex-row xl:max-w-[980px]">
         <div className="w-full pt-8">
           <h1 className="pb-6 text-2xl font-extrabold text-primary transition-all md:gap-3 md:text-3xl ">
-            {`${t('h1')}   `}
-            <Image
-              width={30}
-              height={30}
-              alt="waving hand"
-              src="https://raw.githubusercontent.com/Kielx/Kielx/main/wave.gif"
-            />
+            {`${t('h1')} 👋  `}
           </h1>
           <div className="flex flex-col gap-4 ">
             <p>{t('about1')}</p>
             <p>{t('about2')}</p>
             <p>{t('about3')}</p>
           </div>
+        </div>
+        <div className="animate__animated animate__fadeIn animate__faster relative m-auto flex h-[250px]  w-[250px] flex-shrink-0 flex-grow-0 rounded-md   lg:order-2">
+          <Image
+            src={heroImage}
+            placeholder="blur"
+            layout="fixed"
+            alt="about"
+            width={250}
+            height={250}
+            className="rounded-md"
+            priority
+          />
         </div>
       </div>
     </div>
