c4a84272996bbc222de414f72f0fc632447b3fe4
Kielx <kiel66@gmail.com>
2022-05-14 20:33:40 +0200

    Fix SEO Scores and twitter:cards

--- components/Head.tsx
+++ components/Head.tsx
@@ -41,21 +41,27 @@ const Head = ({
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#2b5797" />
       <meta name="theme-color" content="#ffffff" />
-      <meta name="description" content={description} key="description" />
+      <meta name="description" content={description} />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       {/*  Essential META Tags */}
       <meta property="og:title" content={title} key="title" />
       <meta property="og:type" content="article" />
-      <meta property="og:image" content={image} />
-      <meta property="og:url" content={url} />
-      <meta name="twitter:image" content={image} />
+
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta
+        name="twitter:image"
+        content={image[0] === '/' ? `https://blog.pantak.net${image}` : image}
+      />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image:alt" content={imageAltText} />
       {/*  Non-Essential, But Recommended */}
       <meta property="og:description" key="description" content={description} />
       <meta property="og:site_name" content={siteName} />
-      <meta name="twitter:image:alt" content={imageAltText} />
+      <meta property="og:url" content={url} />
+
       {/*  Non-Essential, But Required for Analytics 
       <meta property="fb:app_id" content="your_app_id" />
       <meta name="twitter:site" content="@website-username" />
